0b1ac54a21cdab6cab899428d704cdb4217d5a19
iain <iainfraser188@msn.com>
2022-01-20 10:38:21 +0000

    inital commit

---
+++ src/main/java/com/codeclan/example/CourseBookingSystem/models/Course.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.CourseBookingSystem.models;
+
+public class Course {
+}
---
+++ src/main/java/com/codeclan/example/CourseBookingSystem/repositories/CustomerRepository.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.CourseBookingSystem.repositories;
+
+public interface CustomerRepository {
+}
